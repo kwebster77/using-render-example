a9ddf37a08becfe5a6c49a6576a6f8ac7403722a
Karley Webster <80624137+kwebster77@users.noreply.github.com>
2023-09-09 02:20:33 +0100

    updated url

--- render-fontend-project/src/Form.js
+++ render-fontend-project/src/Form.js
@@ -19,7 +19,7 @@ function Form({onSubmit}) {
         e.preventDefault();
     
         try {
-          const response = await fetch('https://render-example-form-frontend.onrender.com/api/submit-form', {
+          const response = await fetch('https://render-example-form.onrender.com/api/submit-form', {
             method: 'POST',
             headers: {
               'Content-Type': 'application/json',
